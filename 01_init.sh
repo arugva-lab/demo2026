84cf555ba2fe91ba60a2f911c685e0695e31a3e1
arugva <arugva21@gmail.com>
2025-12-19 02:43:50 +0500

    big fix

--- 01_init.sh
+++ 01_init.sh
@@ -1,137 +1,46 @@
+#!/bin/bash
 s#!/bin/bash
 
+# Подгружаем настройки и библиотеку
 source ./env.sh
 source ./lib.sh
 
-echo ">>> Module <<<"
-
-#   --- 1.1 ISP (Astra) ---
-CMD_ISP_NET='
-hostnamectl set-hostname ISP
-timedatectl set-timezone '$TIMEZONE'
-
-cat > /etc/network/interfaces <<EOF
-auto lo
-iface lo inet loopback
-
-# WAN (DHCP)
-auto '$ISP_IF_WAN'
-iface '$ISP_IF_WAN' inet dhcp
-
-# LAN to HQ
-auto '$ISP_IF_HQ'
-iface '$ISP_IF_HQ' inet static
-    address 172.16.1.1/28
-
-# LAN to BR
-auto '$ISP_IF_BR'
-iface '$ISP_IF_BR' inet static
-    address 172.16.2.1/28
-EOF
-
-sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-
-# NAT
-iptables -t nat -A POSTROUTING -o '$ISP_IF_WAN' -j MASQUERADE
-touch /etc/iptables.rules
-iptables-save >> /etc/iptables.rules
-
-systemctl restart networking
-'
-
-CMD_ISP_USER='
-useradd -m -s /bin/bash '$USER_ADMIN'
-echo "'$USER_ADMIN':'$PASS'" | chpasswd
-echo "'$USER_ADMIN' ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/'$USER_ADMIN'
-'
-
-vm_exec $ID_ISP "$CMD_ISP_NET" "network ISP (Astra)"
-vm_exec $ID_ISP "$CMD_ISP_USER" "create admin ISP"
-
-
-# --- 1.2 HQ-RTR (Astra) ---
-CMD_HQ_RTR='
-hostnamectl set-hostname HQ-RTR
-timedatectl set-timezone '$TIMEZONE'
-modprobe 8021q
-echo "8021q" >> /etc/modules
-
-cat > /etc/network/interfaces <<EOF
-auto lo
-iface lo inet loopback
-
-auto '$HQ_IF_WAN'
-iface '$HQ_IF_WAN' inet static
-    address 172.16.1.2/28
-    gateway 172.16.1.1
-
-# Trunk Port
-auto '$HQ_IF_LAN'
-iface '$HQ_IF_LAN' inet manual
-
-# VLANs
-auto '$HQ_IF_LAN'.100
-iface '$HQ_IF_LAN'.100 inet static
-    address 192.168.1.1/27
-    vlan-raw-device '$HQ_IF_LAN'
-
-auto '$HQ_IF_LAN'.200
-iface '$HQ_IF_LAN'.200 inet static
-    address 192.168.2.1/27
-    vlan-raw-device '$HQ_IF_LAN'
+@@ -83,6 +82,7 @@ iface '$HQ_IF_LAN'.200 inet static
+   vlan-raw-device '$HQ_IF_LAN'
 
 auto '$HQ_IF_LAN'.999
 iface '$HQ_IF_LAN'.999 inet static
 iface '$HQ_IF_LAN'.999 inet static
-    address 192.168.99.1/29
-    vlan-raw-device '$HQ_IF_LAN'
-EOF
-
-sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-systemctl restart networking
-
-useradd -m -s /bin/bash '$USER_ADMIN'
-echo "'$USER_ADMIN':'$PASS'" | chpasswd
-echo "'$USER_ADMIN' ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/'$USER_ADMIN'
-'
-
-vm_exec $ID_HQ_RTR "$CMD_HQ_RTR" "HQ-RTR"
-
-
-# --- 1.3 BR-RTR ---
-CMD_BR_RTR='
-hostnamectl set-hostname BR-RTR
-timedatectl set-timezone '$TIMEZONE'
-
-cat > /etc/network/interfaces <<EOF
-auto lo
-iface lo inet loopback
-
-auto '$BR_IF_WAN'
-iface '$BR_IF_WAN' inet static
-    address 172.16.2.2/28
-    gateway 172.16.2.1
-
-auto '$BR_IF_LAN'
-iface '$BR_IF_LAN' inet static
-    address 192.168.3.1/28
-EOF
-
-sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-systemctl restart networking
-'
-vm_exec $ID_BR_RTR "$CMD_BR_RTR" "BR-RTR"
+   address 192.168.99.1/29
+   vlan-raw-device '$HQ_IF_LAN'
+@@ -127,55 +127,18 @@ vm_exec $ID_BR_RTR "$CMD_BR_RTR" "BR-RTR"
 
 
 #   --- 2.1 HQ-SRV ---
+CMD_HQ_SRV='
+hostnamectl set-hostname HQ-SRV
+timedatectl set-timezone '$TIMEZONE'
+mkdir -p /etc/net/ifaces/'$HQ_SRV_IF'
+echo "TYPE=eth\n DISABLE=no\n ONBOOT=yes\n" > /etc/net/ifaces/'$HQ_SRV_IF'/options
+echo "BOOTPROTO=static" >> /etc/net/ifaces/'$HQ_SRV_IF'/options
+echo "192.168.1.2/27" > /etc/net/ifaces/'$HQ_SRV_IF'/ipv4address
+echo "192.168.1.1" > /etc/net/ifaces/'$HQ_SRV_IF'/ipv4route
+systemctl restart network
+# Настройка пользователя sshuser
+useradd -m -u 2026 -s /bin/bash '$USER_SSH'
+echo "'$USER_SSH':'$PASS'" | chpasswd
+echo "'$USER_SSH' ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/'$USER_SSH'
+'
+vm_exec $ID_HQ_SRV "$CMD_HQ_SRV" "Настройка HQ-SRV (Alt Linux)"
+
+
+# --- 2.2 BR-SRV---
 CMD_BR_SRV='
 hostnamectl set-hostname BR-SRV
 timedatectl set-timezone '$TIMEZONE'
 mkdir -p /etc/net/ifaces/'$BR_SRV_IF'
 echo "TYPE=eth" > /etc/net/ifaces/'$BR_SRV_IF'/options
+echo "BOOTPROTO=static\n ONBOOT=yes\n DISABLE=no" >> /etc/net/ifaces/'$BR_SRV_IF'/options
 echo "BOOTPROTO=static" >> /etc/net/ifaces/'$BR_SRV_IF'/options
 echo "ONBOOT=yes" >> /etc/net/ifaces/'BR_SRV_IF'/options
 echo "DISABLED=no" >> /etc/net/ifaces/'BR_SRVIF'/options
@@ -142,3 +51,24 @@ systemctl restart network
 
 useradd -m -u 2026 -s /bin/bash '$USER_SSH'
 echo "'$USER_SSH':'$PASS'" | chpasswd
+echo "'$USER_SSH' ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/'$USER_SSH'
+'
+
+vm_exec $ID_BR_SRV "$CMD_BR_SRV" "BR-SRV"
+
+# --- 3.1 HQ-CLI ---
+
+CMD_HQ_CLI='
+hostnamectl set-hostname HQ-CLI-01
+timedatectl set-timezone '$TIMEZONE'
+mkdir -p /etc/net/ifaces/'$HQ_CLI_IF'
+echo "TYPE=eth" > /etc/net/ifaces/'$HQ_CLI_IF'/options
+echo "BOOTPROTO=dhcp\n ONBOOT=yes\n DISABLE=no" >> /etc/net/ifaces/'$HQ_CLI_IF'/options
+
+systemctl restart network
+'
+
+vm_exec $ID_HQ_CLI "$CMD_HQ_CLI" "HQ-CLI"
+
+
+echo ">>> MODULE 1 COMPLETE <<<"
