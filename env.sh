d86e86017040456157c2755ef702597646607ab4
arugva-lab <arugva21@gmail.com>
2025-12-18 20:37:05 +0500

    Update env.sh
    мини правки комментариев

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#1. ID ВИРТУАЛЬНЫХ МАШИН (Proxmox VMID)
+#1. ID VIRTUAL MACHINES (Proxmox VMID)
 
 ID_ISP=100
 ID_HQ_RTR=101
@@ -10,24 +10,28 @@ ID_HQ_CLI=104
 ID_BR_SRV=105
 
 
-# 2. НАЗВАНИЯ ИНТЕРФЕЙСОВ (ip a)
+# 2. INTERFACES NAME (ip a)
 
 # --- ISP ---
-ISP_IF_WAN="eth0"   # В интернет (NAT)
-ISP_IF_HQ="eth1"    # В сторону HQ-RTR
-ISP_IF_BR="eth2"    # В сторону BR-RTR
+ISP_IF_WAN="eth0"   # v internet (NAT)
+ISP_IF_HQ="eth1"    # v storonu HQ-RTR
+ISP_IF_BR="eth2"    # v storonu BR-RTR
 
 # --- HQ-RTR ---
-HQ_IF_WAN="eth0"    # В сторону ISP
-HQ_IF_LAN="eth1"    # Внутренняя сеть
+HQ_IF_WAN="eth0"    # v storonu ISP
+HQ_IF_LAN="eth1"    # LAN
 
 # --- BR-RTR ---
-BR_IF_WAN="eth0"    # В сторону ISP
-BR_IF_LAN="eth1"    # Внутренняя сеть
+BR_IF_WAN="eth0"    # v storonu ISP
+BR_IF_LAN="eth1"    # LAN сеть
 
-# --- SERVERS / CLIENTS ---
+# --- HQ-SRV ---
 HQ_SRV_IF="ens18"
+
+# --- HQ-CLI ----
 HQ_CLI_IF="ens18"
+
+# ---BR-SRV ---
 BR_SRV_IF="ens18"
 
 #3. УЧЕТНЫЕ ДАННЫЕ И НАСТРОЙКИ
