73d4a7723101c04d266fbb7fb10f4e7c5e547ce2
arugva-lab <arugva21@gmail.com>
2025-12-19 13:55:28 +0500

    fix \n 01_init.sh
    Вместо попытки сделать отступы при помощи \n просто вписал echo отдельно для каждой строки конфига /ens18/options

--- 01_init.sh
+++ 01_init.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+s#!/bin/bash
 
-# Подгружаем настройки и библиотеку
 source ./env.sh
 source ./lib.sh
 
@@ -84,6 +83,7 @@ iface '$HQ_IF_LAN'.200 inet static
 
 auto '$HQ_IF_LAN'.999
 iface '$HQ_IF_LAN'.999 inet static
+iface '$HQ_IF_LAN'.999 inet static
     address 192.168.99.1/29
     vlan-raw-device '$HQ_IF_LAN'
 EOF
@@ -127,30 +127,14 @@ vm_exec $ID_BR_RTR "$CMD_BR_RTR" "BR-RTR"
 
 
 #   --- 2.1 HQ-SRV ---
-CMD_HQ_SRV='
-hostnamectl set-hostname HQ-SRV
-timedatectl set-timezone '$TIMEZONE'
-mkdir -p /etc/net/ifaces/'$HQ_SRV_IF'
-echo "TYPE=eth\n DISABLE=no\n ONBOOT=yes\n" > /etc/net/ifaces/'$HQ_SRV_IF'/options
-echo "BOOTPROTO=static" >> /etc/net/ifaces/'$HQ_SRV_IF'/options
-echo "192.168.1.2/27" > /etc/net/ifaces/'$HQ_SRV_IF'/ipv4address
-echo "192.168.1.1" > /etc/net/ifaces/'$HQ_SRV_IF'/ipv4route
-systemctl restart network
-# Настройка пользователя sshuser
-useradd -m -u 2026 -s /bin/bash '$USER_SSH'
-echo "'$USER_SSH':'$PASS'" | chpasswd
-echo "'$USER_SSH' ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/'$USER_SSH'
-'
-vm_exec $ID_HQ_SRV "$CMD_HQ_SRV" "Настройка HQ-SRV (Alt Linux)"
-
-
-# --- 2.2 BR-SRV---
 CMD_BR_SRV='
 hostnamectl set-hostname BR-SRV
 timedatectl set-timezone '$TIMEZONE'
 mkdir -p /etc/net/ifaces/'$BR_SRV_IF'
 echo "TYPE=eth" > /etc/net/ifaces/'$BR_SRV_IF'/options
-echo "BOOTPROTO=static\n ONBOOT=yes\n DISABLE=no" >> /etc/net/ifaces/'$BR_SRV_IF'/options
+echo "BOOTPROTO=static" >> /etc/net/ifaces/'$BR_SRV_IF'/options
+echo "ONBOOT=yes" >> /etc/net/ifaces/'BR_SRV_IF'/options
+echo "DISABLED=no" >> /etc/net/ifaces/'BR_SRVIF'/options
 echo "192.168.3.2/28" > /etc/net/ifaces/'$BR_SRV_IF'/ipv4address
 echo "192.168.3.1" > /etc/net/ifaces/'$BR_SRV_IF'/ipv4route
 
@@ -158,24 +142,3 @@ systemctl restart network
 
 useradd -m -u 2026 -s /bin/bash '$USER_SSH'
 echo "'$USER_SSH':'$PASS'" | chpasswd
-echo "'$USER_SSH' ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/'$USER_SSH'
-'
-
-vm_exec $ID_BR_SRV "$CMD_BR_SRV" "BR-SRV"
-
-# --- 3.1 HQ-CLI ---
-
-CMD_HQ_CLI='
-hostnamectl set-hostname HQ-CLI-01
-timedatectl set-timezone '$TIMEZONE'
-mkdir -p /etc/net/ifaces/'$HQ_CLI_IF'
-echo "TYPE=eth" > /etc/net/ifaces/'$HQ_CLI_IF'/options
-echo "BOOTPROTO=dhcp\n ONBOOT=yes\n DISABLE=no" >> /etc/net/ifaces/'$HQ_CLI_IF'/options
-
-systemctl restart network
-'
-
-vm_exec $ID_HQ_CLI "$CMD_HQ_CLI" "HQ-CLI"
-
-
-echo ">>> MODULE 1 COMPLETE <<<"
