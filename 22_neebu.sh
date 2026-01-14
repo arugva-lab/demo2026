171c79d6d2e89f6e2d94f1e5bfb1f18d08babc15
arugva <arugva21@gmail.com>
2026-01-14 16:01:50 +0500

    stable web

--- 22_neebu.sh
+++ 22_neebu.sh
@@ -59,3 +59,34 @@ docker compose -f /root/testapp/docker-compose.yaml up -d
 '
 vm_exec $ID_BR_SRV "$CMD_DOCKER" "test docker"
 
+
+qm set $ID_HQ_SRV -scsi3 local:iso/Additional.iso,media=cdrom
+
+CMD_WEB="
+for host in /sys/class/scsi_host/host*; do
+        echo '- - -' > '$host/scan'
+done
+
+apt-get update && apt-get install lamp-server -y
+mkdir -p /mnt/add_cd/
+mount -t auto -o ro /dev/sr1 /mnt/add_cd
+cp /mnt/add_cd/web/index.php /var/www/html
+cp /mnt/add_cd/web/logo.png /var/www/html
+chown -R apache:apache /var/www/html
+chmod -R 755 /var/www/html
+sed -i 's/\r//g' /var/www/html/index.php
+sed -i 's/\$username = \"user\"/\$username = \"web\";/' /var/www/html/index.php
+sed -i 's/\$password = \"password\"/\$password = \"P@ssw0rd\";/' /var/www/html/index.php
+sed -i 's/\$dbname = \"db\"/\$dbname = \"webdb\";/' /var/www/html/index.php
+systemctl enable --now mariadb
+sleep 22
+mariadb -u root <<'EOF'
+CREATE DATABASE webdb;
+CREATE USER 'web'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON webdb.* TO 'web'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+mariadb -u root webdb < /mnt/add_cd/web/dump.sql
+systemctl enable --now httpd2
+"
+vm_exec $ID_HQ_SRV "$CMD_WEB" "test web"
