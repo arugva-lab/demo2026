7415e5441e826170d2dfd593fb3c1f53b13eb74c
arugva-lab <arugva21@gmail.com>
2026-02-11 21:15:12 +0500

    Update main.sh
    Удалил wget тестовых скриптов

--- main.sh
+++ main.sh
@@ -6,8 +6,7 @@ wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/
 wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/22_neebu.sh
 wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/env.sh
 wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/lib.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/test1.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/test2.sh
+
 
 bash isc-dhcp-server/11_init.sh
 bash isc-dhcp-server/12_routing.sh
