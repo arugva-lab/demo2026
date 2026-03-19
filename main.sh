5feffd437e4c384c07e9261bff71b6e39625e60c
arugva-lab <arugva21@gmail.com>
2026-03-19 09:37:30 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-mkdir isc-dhcp-server
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/11_init.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/12_routing.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/21_xz.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/22_neebu.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/env.sh
-wget -qP isc-dhcp-server/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/lib.sh
-
-
-bash isc-dhcp-server/11_init.sh
-bash isc-dhcp-server/12_routing.sh
-bash isc-dhcp-server/21_xz.sh
-bash isc-dhcp-server/22_neebu.sh
+mkdir /var/tmp/qemu/
+wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/11_init.sh
+wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/12_routing.sh
+wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/21_xz.sh
+wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/22_neebu.sh
+wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/env.sh
+wget -qP /var/tmp/qemu// https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/lib.sh
