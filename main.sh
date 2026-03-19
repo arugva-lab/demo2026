07e1d626bdfb3f0abd6f0a7019bd8c708a7c7765
arugva-lab <arugva21@gmail.com>
2026-03-19 10:16:35 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,3 +6,8 @@ wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/re
 wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/22_neebu.sh
 wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/env.sh
 wget -qP /var/tmp/qemu// https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/lib.sh
+
+chmod +x /var/tmp/qemu/11_init.sh
+chmod +x /var/tmp/qemu/12_routing.sh
+chmod +x /var/tmp/qemu/21_xz.sh
+chmod +x /var/tmp/qemu/22_neebu.sh
