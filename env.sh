f92d4c596537d5380fc963fd162740fcaa071537
arugva-lab <arugva21@gmail.com>
2026-04-27 09:14:39 +0500

    Удалил лишние перменные, очистил значения по умолчанию

--- env.sh
+++ env.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
 
 #1. ID VIRTUAL MACHINES (Proxmox VMID)
+#Virtual machine IDs can be found in the Proxmox interface near the machine name
+ID_ISP=
+ID_HQ_RTR=
+ID_BR_RTR=
+ID_HQ_SRV=
+ID_HQ_CLI=
+ID_BR_SRV=
 
-ID_ISP=100
-ID_HQ_RTR=101
-ID_BR_RTR=102
-ID_HQ_SRV=103
-ID_HQ_CLI=104
-ID_BR_SRV=105
 
-
-# 2. INTERFACES NAME (ip a)
+#2. INTERFACES NAME (ip a)
+#put interface names inside quotes
 
 # --- ISP ---
-ISP_IF_WAN="eth0"   # v internet (NAT)
-ISP_IF_HQ="eth1"    # v storonu HQ-RTR
-ISP_IF_BR="eth2"    # v storonu BR-RTR
+ISP_IF_WAN=""   # v internet (NAT)
+ISP_IF_HQ=""    # v storonu HQ-RTR
+ISP_IF_BR=""    # v storonu BR-RTR
 
 # --- HQ-RTR ---
-HQ_IF_WAN="eth0"    # v storonu ISP
-HQ_IF_LAN="eth1"    # LAN
+HQ_IF_WAN=""    # v storonu ISP
+HQ_IF_LAN=""    # LAN
 
 # --- BR-RTR ---
-BR_IF_WAN="eth0"    # v storonu ISP
-BR_IF_LAN="eth1"    # LAN сеть
+BR_IF_WAN=""    # v storonu ISP
+BR_IF_LAN=""    # LAN сеть
 
 # --- HQ-SRV ---
-HQ_SRV_IF="ens18"
+HQ_SRV_IF=""
 
 # --- HQ-CLI ----
-HQ_CLI_IF="ens18"
+HQ_CLI_IF=""
 
 # ---BR-SRV ---
-BR_SRV_IF="ens18"
-
-#3. УЧЕТНЫЕ ДАННЫЕ И НАСТРОЙКИ
-
-USER_ADMIN="net_admin"
-USER_SSH="sshuser"
-PASS="P@ssw0rd"
-TIMEZONE="Asia/Yekaterinburg"
+BR_SRV_IF=""
