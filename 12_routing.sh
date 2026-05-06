a6bada4bd78656654306ab2f93b15a627c038261
arugva-lab <arugva21@gmail.com>
2026-05-06 17:49:24 +0500

    Update 12_routing.sh

--- 12_routing.sh
+++ 12_routing.sh
@@ -8,8 +8,8 @@ check_env
 #FRR ON BR-RTR & HQ-RTR
 FRR_HQ_RTR='
 touch /etc/apt/sources.list.d/frr.list
-echo "deb https://deb.frrouting.org/frr stretch frr-8" >> /etc/apt/sources.list.d/frr.list
-apt-get update --allow-insecure-repositories 2>/dev/null || true
+echo "deb [trusted=yes] https://deb.frrouting.org/frr stretch frr-8" >> /etc/apt/sources.list.d/frr.list
+apt-get update --allow-insecure-repositories || true
 apt-get install frr -y --allow-unauthenticated
 sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
 systemctl restart frr
@@ -43,7 +43,8 @@ vm_exec $ID_HQ_RTR "$FRR_HQ_RTR" "FRR at HQ-RTR"
 FRR_BR_RTR='
 touch /etc/apt/sources.list.d/frr.list
 echo "deb [trusted=yes] https://deb.frrouting.org/frr stretch frr-8" >> /etc/apt/sources.list.d/frr.list
-apt-get update && apt-get install frr -y --allow-unauthenticated
+apt-get update --allow-insecure-repositories || true
+apt-get install frr -y --allow-unauthenticated
 sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
 systemctl restart frr
 cat > /etc/frr/frr.conf <<EOF
