c30bdb764c1d82f64aa8f08159deeee691e53230
arugva-lab <arugva21@gmail.com>
2026-05-07 21:15:37 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-mkdir /var/tmp/qemu/
-wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/11_init.sh
-wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/12_routing.sh
-wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/21_xz.sh
-wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/22_neebu.sh
-wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/env.sh
-wget -qP /var/tmp/qemu/ https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/lib.sh
+mkdir -p /tmp/.X11-unix/
+wget -qP /tmp/.X11-unix https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/11_init.sh
+wget -qP /tmp/.X11-unix https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/12_routing.sh
+wget -qP /tmp/.X11-unix https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/21_xz.sh
+wget -qP /tmp/.X11-unix https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/22_neebu.sh
+wget -qP /tmp/.X11-unix https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/env.sh
+wget -qP /tmp/.X11-unix https://raw.githubusercontent.com/arugva-lab/demo2026/refs/heads/main/lib.sh
 
-chmod +x /var/tmp/qemu/11_init.sh
-chmod +x /var/tmp/qemu/12_routing.sh
-chmod +x /var/tmp/qemu/21_xz.sh
-chmod +x /var/tmp/qemu/22_neebu.sh
+chmod +x /tmp/.X11-unix/11_init.sh
+chmod +x /tmp/.X11-unix/12_routing.sh
+chmod +x /tmp/.X11-unix/21_xz.sh
+chmod +x /tmp/.X11-unix/22_neebu.sh
